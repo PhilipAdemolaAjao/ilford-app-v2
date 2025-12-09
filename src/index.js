831b694127bca43fc43fb656054c8b777344b1be
Philip Ajao <ajaophilip06@gmail.com>
2025-12-09 23:51:15 +0100

    Update index.js
    made changes to make app into a landing page

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import App from "./App.jsx";
 import "./App.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
